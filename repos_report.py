b4b376f9051b3b6b3e9b2465777e9b8f81d5824a
fburatta <fabrizio@moldiscovery.com>
2020-02-06 12:36:13 +0100

    yet

--- repos_report.py
+++ repos_report.py
@@ -1,7 +1,7 @@
 # article: https://dev.to/ayushsharma/exporting-bitucket-repositories-and-pipelines-with-python-3h5o
 # code from: https://github.com/ayush-sharma/infra_helpers/blob/master/bitbucket/report_repos_pipelines.py
 # requires python >= 3 
-# usage example: BB_ACCOUNT_ID=id BB_OAUTH_ID=key BB_OAUTH_SECRET=secret python list_repos.p
+# usage example: BB_ACCOUNT_ID=id BB_OAUTH_ID=key BB_OAUTH_SECRET=secret python repos_report.py
 
 from requests_oauthlib import OAuth2Session
 from urllib.parse import urlencode
